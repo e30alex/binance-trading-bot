2faf0ebf5e23d38900c1efe75d3c3750f71466dc
Alex Oancea <alex.constantin.oancea@gmail.com>
2026-01-16 11:50:25 +0200

    Add log for ip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as https from 'https';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Log outbound IP for debugging
+  https.get('https://api.ipify.org', (res) => {
+    res.on('data', (d: Buffer) => console.log('Outbound IP:', d.toString()));
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
